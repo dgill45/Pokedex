90026fa0973736f2ca8c520a95bfb775ffa27256
donellgill <donell.gill@outlook.com>
2022-04-13 16:37:38 -0400

    Created Pokedex and Pokecard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react"
+import React, { useState} from "react"
+import Pokedex from "./components/Pokedex.js";
+import "../public/data.js"
 import './App.css';
 
 function App() {
+
+  const [card, setCard] = useState(card)
+
+  function populatePokedex()
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <Pokedex />
        
       </header>
     </div>
